9f978b20b27627af1894e02e3d8e973343e376f3
casas-emilia <casasemiliaweb@gmail.com>
2025-01-15 00:15:04 -0300

    cors ruta frontend

--- routers/routes.go
+++ routers/routes.go
@@ -18,7 +18,7 @@ func SetupRouter() *gin.Engine {
 			"http://localhost:5173",
 			"http://192.168.0.11:3000",
 			"https://v1-backend-casas-charlotte-production.up.railway.app",
-			"https://v1-frontend-casas-charlotte-production.up.railway.app/",
+			"https://v1-frontend-casas-charlotte-production.up.railway.app",
 		}, // Dominios permitidos
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Métodos HTTP permitidos
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Headers permitidos
